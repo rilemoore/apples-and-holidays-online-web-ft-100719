50e449a02982280a7bb7eb206ec4fc465ed04ab7
Riley Moore <rilemoore@gmail.com>
2019-10-15 05:15:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,15 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  winter_supplies = []
+    holiday_hash.each do |season, season_hash|
+    if (season == :winter)
+      season_hash.each do |holiday, supplies|
+        winter_supplies.insert(-1, supplies)
+      end
+    end
+      
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
