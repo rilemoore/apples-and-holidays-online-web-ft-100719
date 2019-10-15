75801edea93104a12e6b29ad02bc5622709cafe0
Riley Moore <rilemoore@gmail.com>
2019-10-15 05:34:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,11 +90,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
     holiday_hash.each do |season, season_hash|
       puts "#{season.to_s.capitalize!}:"
-      #season_hash.each do |holiday, supplies|
-       # puts "#{holiday.to_s.capitalize!}: #{supplies.join().capitalize!}"
-        #supplies.each do |array_contents|
-        #end
-      #end
+      season_hash.each do |holiday, supplies|
+        puts "#{holiday.to_s.capitalize!}: #{supplies.join().capitalize!}"
+      end
     end
 end
 
