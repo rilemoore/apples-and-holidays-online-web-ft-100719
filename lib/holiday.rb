05f101d05d8831ba10a68e9d69add557ec873d7a
Riley Moore <rilemoore@gmail.com>
2019-10-15 04:53:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, season_hash|
     if season == "winter"
       season_hash.each do |holiday, supplies|
-        supplies += [supply]
+        supplies << supply
       end
     end
       
