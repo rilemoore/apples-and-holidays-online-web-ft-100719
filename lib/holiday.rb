e20245bf4a8a93b3af474cbb1b1fa511059fa3cd
Riley Moore <rilemoore@gmail.com>
2019-10-15 04:52:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
   holiday_hash.each do |season, season_hash|
-    if attribute == "winter"
+    if season == "winter"
       season_hash.each do |holiday, supplies|
         supplies += [supply]
       end
